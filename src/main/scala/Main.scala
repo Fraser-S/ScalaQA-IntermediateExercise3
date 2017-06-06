dd3c0f5cf549dd3566210a8c97f20927e2269d43
Fraser <frasers@hotmail.co.uk>
2017-06-06 13:45:55 +0000

    added the tooHot function that returns true depending on the upper temperature and if it is summer or not

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,19 @@
+/**
+  * Created by Fraser on 06/06/2017.
+  */
+
+object Main {
+  def tooHot(temperature : Int, isSummer : Boolean) : Boolean = isSummer match{
+    case x if !isSummer && temperature == 80 => true
+    case x if isSummer && temperature == 100 => true
+    case _ => false
+  }
+
+  def main(args : Array[String]) : Unit = {
+    val summer : Boolean = true
+    val notSummer : Boolean = false
+    println("input(80,false) -> " + tooHot(80, notSummer))
+    println("input(100,false) -> " + tooHot(100, notSummer))
+    println("input(100,false) -> " + tooHot(100, summer))
+  }
+}
